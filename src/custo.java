e3470a3b60662bd3d44843976f84605f4eb791d6
Lucas Augusto <lucasamgs@gmail.com>
2020-05-29 20:25:16 -0300

    Leitura dos dados

--- src/custo.java
+++ src/custo.java
@@ -1,8 +1,20 @@
-
+import java.util.Locale;
+import java.util.Scanner;
 public class custo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+		int horas,thoras,opçmen;
+		double vph,custo,caro1,caro2;
+		char opçao;
+		
+		
+		
+		
+		sc.close();
 
 	}
 
