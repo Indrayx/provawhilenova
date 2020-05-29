50618d01d202aee3a24035031dd219b9a4c922ec
Lucas Augusto <lucasamgs@gmail.com>
2020-05-29 20:28:19 -0300

    Menu repetindo

--- src/custo.java
+++ src/custo.java
@@ -10,6 +10,45 @@ public class custo {
 		int horas,thoras,opçmen;
 		double vph,custo,caro1,caro2;
 		char opçao;
+		thoras=0;
+		custo=0;		
+		
+		System.out.print("Nome:");
+		nome = sc.next();
+		System.out.print("Horas trabalhadas:");
+		horas = sc.nextInt();
+		System.out.print("Valor por hora:");
+		vph = sc.nextDouble();
+		thoras=thoras + horas;
+		custo = custo + (vph*horas);
+		System.out.print("digitar outro(S/N)?");
+		opçao = sc.next().charAt(0);
+		
+		while (opçao == 's' || opçao == 'S') { 
+			System.out.print("Nome:");
+			nome = sc.next();
+			System.out.print("Horas trabalhadas:");
+			horas = sc.nextInt();
+			System.out.print("Valor por hora:");
+			vph = sc.nextDouble();
+			System.out.print("digitar outro(S/N)?");
+			opçao = sc.next().charAt(0);
+			thoras=thoras + horas;
+			custo = custo + (vph*horas);
+			
+		}
+		System.out.println("MENU");
+		System.out.println("1 - Total de horas trabalhadas");
+		System.out.println("2 - Custo total");
+		System.out.println("3 - Nome da pessoa que ganhou mais");
+		System.out.println("4 - Sair");
+		System.out.print("Digite uma opção:");
+		opçmen = sc.nextInt();
+		
+		
+		if(opçmen == 4) {
+			System.out.println("FIM DO PROGRAMA!");
+		}
 		
 		
 		
