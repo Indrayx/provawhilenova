be13721338f2dad671cbb84cd4edb00784df8473
Lucas Augusto <lucasamgs@gmail.com>
2020-05-29 20:29:35 -0300

    Tratativas 1 e 2

--- src/custo.java
+++ src/custo.java
@@ -45,6 +45,43 @@ public class custo {
 		System.out.print("Digite uma opção:");
 		opçmen = sc.nextInt();
 		
+while(opçmen != 4) {
+			
+			if(opçmen == 1) {
+			System.out.println(" Total de horas = " + thoras);
+			System.out.println("MENU");
+			System.out.println("1 - Total de horas trabalhadas");
+			System.out.println("2 - Custo total");
+			System.out.println("3 - Nome da pessoa que ganhou mais");
+			System.out.println("4 - Sair");
+			System.out.print("Digite uma opção:");
+			opçmen = sc.nextInt();
+							
+						
+				
+			}
+			if(opçmen == 2) {
+				System.out.printf("Custo total = R$%.2f%n", custo);
+				System.out.println("MENU");
+				System.out.println("1 - Total de horas trabalhadas");
+				System.out.println("2 - Custo total");
+				System.out.println("3 - Nome da pessoa que ganhou mais");
+				System.out.println("4 - Sair");
+				System.out.print("Digite uma opção:");
+				opçmen = sc.nextInt();
+				
+				
+			}
+			if (opçmen > 4 || opçmen < 0 ) {
+				System.out.println("MENU");
+				System.out.println("1 - Total de horas trabalhadas");
+				System.out.println("2 - Custo total");
+				System.out.println("3 - Nome da pessoa que ganhou mais");
+				System.out.println("4 - Sair");
+				System.out.print("Digite uma opção:");
+				opçmen = sc.nextInt();
+			}
+		
 		
 		if(opçmen == 4) {
 			System.out.println("FIM DO PROGRAMA!");
